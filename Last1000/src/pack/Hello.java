5cb215171b69745ebe76b8561982c14fba584b0c
82103 <82103@DESKTOP-F2HLHM5>
2021-05-28 22:17:12 +0900

    1

--- Last1000/src/pack/Hello.java
+++ Last1000/src/pack/Hello.java
@@ -1,7 +1,10 @@
 package pack;
 
 public class Hello {
-	//해당 프로젝트 우클릭 > Team >Share Project
+	//해당 프로젝트 우클릭 > Team >Share Project > 
+	
+	//위에 진행 후 프로젝트 우클릭 >Team > Commit > 아래 Git staging > "++"아이콘 선택하면
+	//전부 내려감 > Commit Messge에 임의의 메세지 입력 후 commit and push > 
 	public static void main(String[] args) {
 		System.out.println(1);
 
